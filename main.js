9d1606afa35fee9c766125e4bdb44fe8c6923dd0
DozenDevil <ivashka-seregin@mail.ru>
2025-11-05 20:43:53 +0300

    Added "Add Item" button

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+let recipe = []
+document.getElementById('addItem').addEventListener('click', function() {
+    const itemName = document.getElementById('itemName');
+    const itemAmount = document.getElementById('itemAmount');
+    const itemMeasure = document.getElementById('itemMeasure');
+
+    if (!itemName.value) {
+        alert('Введите название ингредиента!');
+        return false;
+    }
+
+    recipe.push({
+        "name": itemName.value,
+        "amount": +itemAmount.value,
+        "measure": itemMeasure.value
+    });
+
+    let ingredient
+    if (+itemAmount.value) {
+        ingredient = `${itemName.value} - ${itemAmount.value} ${itemMeasure.value}`;
+    } else {
+        ingredient = `${itemName.value} - по вкусу`;
+    }
+
+    let itemContainer = document.createElement('div');
+    itemContainer.innerHTML = `
+        <div class="d-flex justify-ingredient">
+            <div>${ingredient}</div>
+            <button class="dangerButton" data-ingredient-name="${itemName.value}">&times;</button>
+        </div>
+    `
+
+    document.getElementById('recipe1').append(itemContainer);
+
+    itemName.value = '';
+    itemAmount.value = '';
+})
+
+// Размер шрифта
+
 if (!localStorage.getItem('userFontSize')) {
     localStorage.setItem('userFontSize', 12);
 }
