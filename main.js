6d9dce38a0c20a4a52af60ef5b739fb6d5d4b1d8
DozenDevil <ivashka-seregin@mail.ru>
2025-11-06 21:21:21 +0300

    Added recipe scaling logic

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ document.getElementById('fsize').addEventListener('click', e => {
 
 // Добавление ингредиента
 
-let recipe = []
+let initialRecipe = []
 document.getElementById('addItem').addEventListener('click', function () {
     const itemName = document.getElementById('itemName');
     const itemAmount = document.getElementById('itemAmount');
@@ -40,7 +40,7 @@ document.getElementById('addItem').addEventListener('click', function () {
         return false;
     }
 
-    recipe.push({
+    initialRecipe.push({
         "name": itemName.value,
         "amount": +itemAmount.value,
         "measure": itemMeasure.value
@@ -61,7 +61,7 @@ document.getElementById('addItem').addEventListener('click', function () {
         </div>
     `
 
-    document.getElementById('recipe').append(itemContainer);
+    document.getElementById('initialRecipe').append(itemContainer);
 
     itemName.value = '';
     itemAmount.value = '';
@@ -69,19 +69,97 @@ document.getElementById('addItem').addEventListener('click', function () {
 
 // Удаление ингредиента
 
-document.getElementById('recipe').addEventListener('click', e => {
+document.getElementById('initialRecipe').addEventListener('click', e => {
     if (!e.target.dataset.ingredientName) {
-        return;
+        return false;
     }
 
     for (let i = 0; i < recipe.length; i++) {
-        if (recipe[i].name == e.target.dataset.ingredientName) {
+        if (recipe[i].name === e.target.dataset.ingredientName) {
             recipe.splice(i, 1);
         }
     }
 
     e.target.closest('.d-flex').remove();
 });
+
+// Вычисление пропорций
+
+document.getElementById('scaleRecipe').addEventListener('click', function () {
+    const scalingDirection = document.getElementById('scalingDirection').value;
+    const scalingCoefficient = document.getElementById('scalingCoefficient').value;
+
+    if (!scalingCoefficient) {
+        alert('Введите размер пропорции!');
+        return false;
+    }
+
+    let scaledRecipe = [];
+
+    if (scalingDirection === "up") {
+        for (ingredient of initialRecipe) {
+            scaledRecipe.push({
+                "name": ingredient.name,
+                "amount": ingredient.amount * scalingCoefficient,
+                "measure": ingredient.measure
+            });
+        }
+    }
+
+    if (scalingDirection === "down") {
+        for (ingredient of initialRecipe) {
+            scaledRecipe.push({
+                "name": ingredient.name,
+                "amount": Math.round(ingredient.amount / scalingCoefficient * 100) / 100,
+                "measure": ingredient.measure
+            });
+        }
+    }
+
+    appendFinalRecipe(scaledRecipe);
+});
+
+function appendFinalRecipe(recipeArray) {
+    const recipeBody = document.getElementById('recipeBody');
+    recipeBody.innerHTML = '';
+
+    for (ingredient of recipeArray) {
+        if (ingredient.amount) {
+            recipeItem = `${ingredient.name} - ${ingredient.amount} ${ingredient.measure}`;
+        } else {
+            recipeItem = `${ingredient.name} - по вкусу`;
+        }
+
+        let itemContainer = document.createElement('div');
+        itemContainer.innerHTML = `
+            <div class="d-flex">
+                <div>${recipeItem}</div>
+            </div>
+        `
+
+        recipeBody.append(itemContainer);
+    }
+}
+
+// Педантичный текст
+
+document.getElementById('scalingCoefficient').addEventListener('input', e => {
+    const number = e.target.value;
+
+    const isIndirect = (
+        (number % 100 < 10 || number % 100 > 20) &
+        (number % 10 == 2 ||
+        number % 10 == 3 ||
+        number % 10 == 4)
+    );
+
+    if (isIndirect) {
+        document.getElementById('pedanticText').innerText = "раза";
+    }
+    else {
+        document.getElementById('pedanticText').innerText = "раз";
+    }
+})
 
 // Изменение названия рецепта
 
@@ -95,7 +173,11 @@ document.getElementById('recipeTitle').addEventListener('click', e => {
 // Копирование рецепта
 
 document.getElementById('copyRecipe').addEventListener('click', function () {
-    let textToCopy = document.getElementById('scaledRecipe').innerText;
+    if (!document.getElementById('recipeBody').innerHTML) {
+        appendFinalRecipe(initialRecipe);
+    }
+
+    let textToCopy = document.getElementById('finalRecipe').innerText;
 
     navigator.clipboard.writeText(textToCopy)
         .then(() => {
